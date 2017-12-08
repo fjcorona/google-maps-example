8348ae2ddd872873bff433a7815722ca65821f7b
fjcorona <fjavier.corona@hotmail.com>
2017-12-08 14:23:26 -0600

    netbeans

---
+++ public_html/js/functions.js
@@ -0,0 +1,45 @@
+var FNC = {};
+FNC = (function() {
+
+    function withoutArrayObjAND(arrayObj, withoutObj) {
+        var length, newLength;
+        do {
+            length = arrayObj.length;
+            arrayObj = _.without(
+                arrayObj, 
+                _.findWhere(
+                    arrayObj, 
+                    withoutObj
+                )
+            );
+            newLength = arrayObj.length;
+        } while(length !== newLength);
+        return arrayObj;
+    }
+        
+    function withoutArrayObjOR(arrayObj, withoutObj) {
+        pairs = _.pairs(withoutObj);
+        withoutArrayObj = [];
+        for(var idx=0; idx < pairs.length; idx++) {
+            withoutArrayObj[idx] = new Object();
+            withoutArrayObj[idx][pairs[idx][0]] = pairs[idx][1];
+        }
+        for(idx = 0; idx < withoutArrayObj.length ; idx++) {
+            arrayObj = withoutArrayObjAND(arrayObj, withoutArrayObj[idx]);
+        }
+        return arrayObj;
+    }
+
+    function avgArrayObjByKey(arrayObj, numKey) {
+        var sum, length;
+        length = arrayObj.length;
+        sum = sumArrayObjByKey(arrayObj, numKey);
+        return sum/length;
+    }
+    
+    return {
+        withoutArrayObjAND : withoutArrayObjAND,
+         withoutArrayObjOR : withoutArrayObjOR,
+          avgArrayObjByKey : avgArrayObjByKey
+    }
+}());
---
+++ public_html/js/performance.js
@@ -0,0 +1,94 @@
+/* ------------------------------------------------------ *\
+    [Methods] Performance Map
+\* ------------------------------------------------------ */
+var mapsPerformance = {
+    cleanLocations: function (locations) {
+        var locationsClean;
+        locationsClean = new Object();
+        locationsClean = [];
+        locationsClean = FNC.withoutArrayObjOR(locations, {
+            "latitud": "", 
+            "longitud": ""
+        });
+        locationsClean = FNC.withoutArrayObjOR(locationsClean, {
+            "latitud": "0", 
+            "longitud": "0"
+        });
+        locationsClean = FNC.withoutArrayObjOR(locationsClean, {
+            "latitud": 0, 
+            "longitud": 0
+        });
+        return locationsClean;
+    },
+    loadMap: function(locations, areaRender, title, active) {
+        locations = mapsPerformance.cleanLocations(locations);
+        active = (active === true) ? true : false;
+        var options, map, markers, bounds, infoWindows;
+        // Reset Area Render
+        $("#" + areaRender).html("");
+        $("#" + areaRender).removeAttr("style");
+        $("#" + areaRender).css("height", "55px");;
+        // Init Arrays
+        markers = new Array();
+        infoWindows = new Array();
+        bounds = new google.maps.LatLngBounds();
+        // If there are maps
+        if(locations.length) {
+            $("#" + areaRender).css("height", "650px");
+            // Make Options
+            options = {
+                center: new google.maps.LatLng(
+                    FNC.avgArrayObjByKey(locations, "latitud"),
+                    FNC.avgArrayObjByKey(locations, "longitud")
+                ),
+                zoom: 16,
+                scrollwheel: false,
+                mapTypeControlOptions: {
+                    mapTypeIds: [
+                        google.maps.MapTypeId.ROADMAP, 
+                        "map_style"
+                    ]
+                }
+            }
+            // Generate Map with options
+            map = new google.maps.Map(
+                document.getElementById(areaRender), 
+                options
+            );
+
+            // Properties Array
+            for(var idx = 0; idx < locations.length; idx++) {
+                // marcador en el centro del mapa
+                markers[idx] = new google.maps.Marker({
+                    position: new google.maps.LatLng(
+                        locations[idx].latitud, 
+                        locations[idx].longitud
+                    ),
+                    map: map,
+                    icon: "img/custom_pin.png",
+                    title: title,
+                    animation: google.maps.Animation.DROP
+                });
+                bounds.extend(markers[idx].getPosition());
+                infoWindows[idx] = new google.maps.InfoWindow({
+                    // content: FNC.getTemplate(temps["map_infobox"], locations[idx])
+                    content: "<div>Ubicación actual</di>"
+                });
+                attachInfoWindowToMarker(map, markers[idx], infoWindows[idx], active);
+            }
+            //
+            map.fitBounds(bounds);
+        } else {
+            $("#" + areaRender).html("<div>No locations found</div>");
+        }
+        // function to attach infowindow with marker
+        function attachInfoWindowToMarker(map, marker, infoWindow, active) {
+            if(active) {
+                infoWindow.open(map, marker);
+            }
+            google.maps.event.addListener(marker, "click", function() {
+                infoWindow.open(map, marker);
+            });
+        }
+    }
+}
